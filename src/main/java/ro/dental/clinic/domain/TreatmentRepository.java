47882e69cd85594cbf78420756848b31d6adc91c
raluca_timonea <timonea_raluca@yahoo.com>
2023-03-03 23:20:06 +0200

    stergere tabela de tratamente

--- src/main/java/ro/dental/clinic/domain/TreatmentRepository.java
+++
@@ -1,6 +0,0 @@
-package ro.dental.clinic.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TreatmentRepository extends JpaRepository<TreatmentEty, Long> {
-}
