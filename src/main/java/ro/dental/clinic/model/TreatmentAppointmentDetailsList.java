0de297c5c29d8c8810d005e651a244811bf5540e
raluca_timonea <timonea_raluca@yahoo.com>
2023-05-17 22:45:36 +0300

    adaugare vizualizare tratamente pentru un pacient

---
+++ src/main/java/ro/dental/clinic/model/TreatmentAppointmentDetailsList.java
@@ -0,0 +1,9 @@
+package ro.dental.clinic.model;
+
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class TreatmentAppointmentDetailsList {
+    private List<TreatmentAppointmentDetailsListItem> items;
+}
---
+++ src/main/java/ro/dental/clinic/model/TreatmentAppointmentDetailsListItem.java
@@ -0,0 +1,11 @@
+package ro.dental.clinic.model;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class TreatmentAppointmentDetailsListItem {
+    private LocalDate date;
+    private String treatment;
+    private EmployeeDetailsListItem doctorDetails;
+}
